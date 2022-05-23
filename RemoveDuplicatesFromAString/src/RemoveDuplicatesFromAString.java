925812ee47150deb1331c4db653eec562bd82dfe
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2022-05-23 13:31:58 +0530

    missed files

--- RemoveDuplicatesFromAString/src/RemoveDuplicatesFromAString.java
+++ RemoveDuplicatesFromAString/src/RemoveDuplicatesFromAString.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class RemoveDuplicatesFromAString {
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringJoiner;
+
+public class RemoveDuplicatesFromAString {
+    public static String removeDuplicates(String str) {
+            char [] chArray = str.toCharArray();
+            StringBuilder sb = new StringBuilder();
+            Set<Character> chHashSet = new HashSet<>();
+
+            for (char c : chArray) {
+                if(chHashSet.add(c)) {
+                    sb.append(c);
+                }
+            }
+            return sb.toString();
+    }
+
+    public static String removeDuplicatesv2(String str) {
+        char[] chArray = str.toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < chArray.length; i++) {
+            if(sb.indexOf(String.valueOf(i)) == -1){
+                sb.append(i);
+            }
+
+        }
+        return sb.toString();
+    }
 }
--- RemoveDuplicatesFromAString/src/RemoveDuplicatesFromAStringStream.java
+++ RemoveDuplicatesFromAString/src/RemoveDuplicatesFromAStringStream.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class RemoveDuplicatesFromAStringStream {
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class RemoveDuplicatesFromAStringStream {
+    public static String removeDuplicates(String str) {
+        return Arrays.asList(str.split("")).stream()
+                .distinct()
+                .collect(Collectors.joining());
+    }
 }
